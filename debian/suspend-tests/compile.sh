576969c507b50202e7ae34c4e02a2ec368c7b03a
Jonas Meurer <jonas@freesources.org>
2019-12-03 13:29:43 +0100

    Remove obsolete cryptsetup-suspend debugging command

--- debian/suspend-tests/compile.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-x86_64-linux-gnu-gcc -o suspend-race-reproducer suspend-race-reproducer.c -g \
-                -O2 -fstack-protector-strong -Wformat -Werror=format-security \
-                -Wdate-time -D_FORTIFY_SOURCE=2 -Wl,-z,relro -Wl,-z,now \
-                --pedantic && 
-    scp suspend-race-reproducer cryptsetup-suspend:~/
