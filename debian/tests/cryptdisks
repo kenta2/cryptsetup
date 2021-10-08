kenta2/cryptsetup

commit c4ae846a2ddd9686143a261563a3012b66f58795

blkid/un_blkid checks: Honor offset= option.
Closes: #994056

debian/checks/blkid
@@ -15,8 +15,9 @@ fi
 
 dev="$1"
 fs="$2"
+ofs="$3"
 
-blkid="$(/sbin/blkid -o value -s TYPE -p -- "$dev")"
+blkid="$(/sbin/blkid -o value -s TYPE -p ${ofs:+-O "$ofs"} -- "$dev")"
 
 # blkid output is empty if $dev has an unknown filesystem
 if [ -z "$blkid" ] && [ -z "$fs" ]; then

debian/checks/un_blkid
@@ -14,8 +14,9 @@ fi
 
 dev="$1"
 fs="$2"
+ofs="$3"
 
-blkid="$(/sbin/blkid -o value -s TYPE -p -- "$dev")"
+blkid="$(/sbin/blkid -o value -s TYPE -p ${ofs:+-O "$ofs"} -- "$dev")"
 
 # blkid output is empty if $dev has an unknown filesystem
 if [ -n "$blkid" ] && [ -z "$fs" ]; then

debian/cryptdisks-functions
@@ -114,8 +114,8 @@ setup_mapping() {
         # fail if the device has a filesystem and the disk encryption format doesn't
         # verify the key digest (unlike LUKS); unless it's swap, otherwise people can't
         # easily convert an existing plainttext swap partition to an encrypted one
-        if ! out="$(/lib/cryptsetup/checks/un_blkid "$CRYPTTAB_SOURCE" 2>/dev/null)" &&
-                ! /lib/cryptsetup/checks/blkid "$CRYPTTAB_SOURCE" swap >/dev/null; then
+        if ! out="$(/lib/cryptsetup/checks/un_blkid "$CRYPTTAB_SOURCE" "" "$CRYPTTAB_OPTION_offset" 2>/dev/null)" &&
+                ! /lib/cryptsetup/checks/blkid "$CRYPTTAB_SOURCE" swap "$CRYPTTAB_OPTION_offset" >/dev/null; then
             log_warning_msg "$CRYPTTAB_NAME: the precheck for '$CRYPTTAB_SOURCE' failed: $out"
             return 1
         fi
@@ -145,8 +145,8 @@ setup_mapping() {
             continue
         fi
         if [ "${CRYPTTAB_OPTION_swap+x}" ]; then
-            if out="$(/lib/cryptsetup/checks/un_blkid "$tmpdev" 2>/dev/null)" ||
-                    /lib/cryptsetup/checks/blkid "$tmpdev" swap >/dev/null 2>&1; then
+            if out="$(/lib/cryptsetup/checks/un_blkid "$tmpdev" "" "$CRYPTTAB_OPTION_offset" 2>/dev/null)" ||
+                    /lib/cryptsetup/checks/blkid "$tmpdev" swap "$CRYPTTAB_OPTION_offset" >/dev/null 2>&1; then
                 mkswap "$tmpdev" >/dev/null 2>&1
             else
                 log_warning_msg "$target: the check for '$CRYPTTAB_NAME' failed. $CRYPTTAB_NAME contains data: $out"

commit 2373709bb461a71a7af46e7e9c59355fce63e52e

blkid/un_blkid checks: Convert offset= value to bytes.
crypttab(5) and cryptsetup(8) use 512 byte sectors, but blkid(8) wants
bytes…

Regression from c4ae846a2ddd9686143a261563a3012b66f58795.

debian/checks/blkid
@@ -17,7 +17,7 @@ dev="$1"
 fs="$2"
 offset="$3"
 
-blkid="$(/sbin/blkid -o value -s TYPE -p ${offset:+-O "$offset"} -- "$dev")"
+blkid="$(/sbin/blkid -o value -s TYPE -p ${offset:+-O "$((offset*512))"} -- "$dev")"
 
 # blkid output is empty if $dev has an unknown filesystem
 if [ -z "$blkid" ] && [ -z "$fs" ]; then

debian/checks/un_blkid
@@ -16,7 +16,7 @@ dev="$1"
 fs="$2"
 offset="$3"
 
-blkid="$(/sbin/blkid -o value -s TYPE -p ${offset:+-O "$offset"} -- "$dev")"
+blkid="$(/sbin/blkid -o value -s TYPE -p ${offset:+-O "$((offset*512))"} -- "$dev")"
 
 # blkid output is empty if $dev has an unknown filesystem
 if [ -n "$blkid" ] && [ -z "$fs" ]; then

debian/tests/cryptdisks
@@ -195,6 +195,15 @@ cat >/etc/crypttab <<-EOF
 	offset_crypt $CRYPT_DEV /dev/urandom plain,cipher=aes-cbc-essiv:sha256,size=256,offset=$offset,skip=$skip
 EOF
 
+# having an existing file system before the offset has no effect (cf. #994056)
+dmsetup create hidden --table "0 $offset linear $CRYPT_DEV 0"
+mke2fs -t ext2 -m0 -Fq /dev/mapper/hidden
+u="$(blkid -p -s UUID -o value /dev/mapper/hidden)"
+dd if=/dev/mapper/hidden of="$TMPDIR/hidden.img" bs=512
+dmsetup remove hidden
+u2="$(blkid -p -s UUID -o value -- "$CRYPT_DEV")"
+test "$u" = "$u2"
+
 cryptdisks_start offset_crypt
 test -b /dev/mapper/offset_crypt
 
@@ -206,8 +215,10 @@ skip2="$(  dmsetup table --target=crypt offset_crypt | cut -d" " -f6)" && test $
 dd if=/dev/zero of=/dev/mapper/offset_crypt bs=1M || true
 cryptdisks_stop offset_crypt
 
-! xxd -l     $((offset*512)) -ps -c32 <"$CRYPT_DEV" | grep -vFx  0000000000000000000000000000000000000000000000000000000000000000
-  xxd -l32 -s$((offset*512)) -ps -c32 <"$CRYPT_DEV" | grep -vFxq 0000000000000000000000000000000000000000000000000000000000000000
+dd if="$CRYPT_DEV" of="$TMPDIR/hidden2.img" bs=512 count="$offset"
+command diff -q -- "$TMPDIR/hidden.img" "$TMPDIR/hidden2.img" || exit 1
+! xxd -l32 -s$((offset*512)) -ps -c32 <"$CRYPT_DEV" | grep -Fxq 0000000000000000000000000000000000000000000000000000000000000000
+rm -f -- "$TMPDIR/hidden.img" "$TMPDIR/hidden2.img"
 
 
 #######################################################################
@@ -559,6 +570,25 @@ EOF
 mke2fs -t ext4 -m0 -Fq -- "$CRYPT_DEV"
 t="$(blkid -s TYPE -o value -- "$CRYPT_DEV")"
 test "$t" = "ext4"
+! cryptdisks_start check_crypt
+test ! -b /dev/mapper/check_crypt
+
+# precheck failed: $CRYPT_DEV contains a filesystem at the given offset (cf. #994056)
+offset=2048
+disk_setup
+cat >/etc/crypttab <<-EOF
+	check_crypt $CRYPT_DEV /dev/urandom plain,cipher=aes-xts-plain64,size=256,offset=$offset
+EOF
+
+dmsetup create hidden --table "0 4096 linear $CRYPT_DEV $offset"
+mke2fs -t ext2 -m0 -Fq /dev/mapper/hidden
+u="$(blkid -p -s UUID -o value /dev/mapper/hidden)"
+dmsetup remove hidden
+u2="$(blkid -p -O$((offset*512)) -s UUID -o value -- "$CRYPT_DEV")"
+test "$u" = "$u2"
+t="$(blkid  -p -O$((offset*512)) -s TYPE -o value -- "$CRYPT_DEV")"
+test "$t" = "ext2"
+
 ! cryptdisks_start check_crypt
 test ! -b /dev/mapper/check_crypt
 